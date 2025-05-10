168c53bfa705c707d0516babfd184a104bbca2e9
Dhanuka <2022t01555@stu.cmb.ac.lk>
2025-05-10 09:12:23 +0530

    Implemented createQueue function

---
+++ caesar_cipher.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "caesar_cipher.h"
+
+// Initialize Circular queue
+char queue[MAXSIZE];
+
+// Create circular queue with A-Z
+void createQueue() {
+    for (int i = 0; i < MAXSIZE; i++) {
+        queue[i] = 'A' + i;
+    }
+}
+
+
+
