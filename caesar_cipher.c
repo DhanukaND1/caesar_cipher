c8950a38eaf98ff2a364bec4a6a7ca6ef0ae8d7d
HashiniHettiarachchi <167964475+HashiniHettiarachchi@users.noreply.github.com>
2025-05-14 08:01:12 +0530

    Implemented decrypt full message functions

--- caesar_cipher.c
+++ caesar_cipher.c
@@ -21,3 +21,11 @@ char decryptCharacter(char character, int shiftKey) {
     int position = (character - 'A' - shiftKey + MAXSIZE) % MAXSIZE;
     return queue[position];
 }
+
+// Decrypt full message
+void decryptMessage(char *message, int shiftKey, char *result) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        result[i] = decryptCharacter(message[i], shiftKey);
+    }
+    result[strlen(message)] = '\0';
+}
